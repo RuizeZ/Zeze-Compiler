417ff3548a9172254a4cc87cd651670494ff53ff
RuizeZ <imruize@126.com>
2025-01-05 13:49:57 +0800

    added compile_process_create

--- compiler.h
+++ compiler.h
@@ -1,4 +1,20 @@
 #ifndef ZEZECOMPILER_H
 #define ZEZECOMPILER_H
-int compile_file(const char* filename, const char* out_filename, int flags);
+
+#include <stdio.h>
+struct compile_process
+{
+    // indicating how this file should be compiled
+    int flags;
+    struct compile_process_input_file
+    {
+        FILE *fp;
+        const char *abs_path;
+    } cfile;
+
+    FILE *ofile;
+};
+
+int compile_file(const char *filename, const char *out_filename, int flags);
+struct compile_provess *compile_process_create(const char *filename, const char *filename_out, int flags);
 #endif
